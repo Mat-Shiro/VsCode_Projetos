451828e06552eac492ab697f2446d2ba2dc3692f
Matheus Jacques <ml.jacques@hotmail.com>
2018-03-19 14:34:50 -0300

    Adicionado o icone do menu

--- React-Native/MenuPersonalizado/HomeScreen.js
+++ React-Native/MenuPersonalizado/HomeScreen.js
@@ -11,6 +11,13 @@ class HomeScreen extends Component{
     render(){
         return (
             <Container>
+                <Header>
+                    <Left>
+                        <Icon name="ios-menu" onPress={ () =>
+                            this.props.navigation.navigate('DrawerOpen')}
+                        />
+                    </Left>
+                </Header>
                 <Content contentContainerStyle={{
                     flex: 1,
                     alignItems: 'center',
--- React-Native/MenuPersonalizado/SettingsScreen.js
+++ React-Native/MenuPersonalizado/SettingsScreen.js
@@ -11,6 +11,14 @@ class SettingsScreen extends Component{
     render(){
         return (
             <Container>
+
+                <Header>
+                    <Left>
+                        <Icon name="ios-menu" onPress={ () =>
+                            this.props.navigation.navigate('DrawerOpen')}
+                        />
+                    </Left>
+                </Header>
                 <Content contentContainerStyle={{
                     flex: 1,
                     alignItems: 'center',
